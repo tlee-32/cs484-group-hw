616c10d6261c8e0ea86fb1d9c856b36a7d18c3f5
Tommy Lee <tlee32@gmu.edu>
2018-09-28 23:21:44 -0400

    Refactored code for CV and sparse matrix operations

--- HW2_Lee_McCullen/src/crossvalidation/crossvalidation.py
+++ HW2_Lee_McCullen/src/crossvalidation/crossvalidation.py
@@ -1,4 +1,49 @@
+import numpy as np
 from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
 
-def logRegCrossValidation(logRegClassifier, tokens, labels, folds, scoringMethod):
-    return cross_val_score(logRegClassifier, tokens, labels, cv=folds, scoring=scoringMethod)
+"""
+    Perform cross-validation on given selection of l1 and l2 solvers.
+
+    return - f1 scores of each solver
+"""
+def logRegCVScore(classifierPipeline, solverMap, sMatrix, labels):
+    scores = []
+    """
+        Test each type of solver (numerical optimizers) with its associated penalty.
+        sag and saga tend to have convergence warnings (tested up to max_iter=5000 (default is 100))
+    """
+    for penalty, solvers in solverMap.items():
+        for s in solvers:
+            print("\nSOLVER: " + penalty + " " + s)
+            logReg = LogisticRegression(solver=s, penalty=penalty, max_iter=10000, multi_class='ovr')
+
+            # Add classification to pipeline and train it
+            classifierPipeline.steps.append(['classification', logReg])
+            classifierPipeline.fit(sMatrix, labels)
+            
+            # Compute cross-validation scores
+            avg = computeCVAverageScore(classifierPipeline, sMatrix, labels)
+            
+            print("avg score from 5x2 cv: %f\n" % avg)
+            scores.append((penalty + " " + s, avg))
+
+            # Reset current logReg classifier for the next solver
+            classifierPipeline.steps.pop() 
+            #print(clf.score(sMatrix, labels)) #<- seems to always return 1.0, may have to do with UndefinedMetricWarning
+    scores = sorted(scores, key=lambda x: x[1]) # rank from best to worst solvers
+    return scores
+
+"""
+    sets x folds (default: 5x2) cross validation.
+    Default parameters compute the average of 5 sets of 2-fold CVs.
+
+    return - overall average of total set averages
+"""
+def computeCVAverageScore(classifierPipeline, sMatrix, labels, sets=5, folds=2, scoring='f1'):
+    avgs = []
+    for _ in range(5):
+        scores = cross_val_score(classifierPipeline, sMatrix, labels, cv=2, scoring='f1')
+        avgs.append( np.average(scores) )
+    print("avgs: " + str(avgs))
+    return np.average(avgs)
--- HW2_Lee_McCullen/src/moleculeclassifier.py
+++ HW2_Lee_McCullen/src/moleculeclassifier.py
@@ -1,86 +1,37 @@
+# Library imports
 import smart_open
-from preprocess.filetokenizer import readRows
-from crossvalidation.crossvalidation import *
-from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
-from sklearn.svm import LinearSVC
-from sklearn.ensemble import ExtraTreesClassifier
-from sklearn.feature_selection import SelectFromModel, VarianceThreshold, SelectKBest, chi2
-from sklearn.pipeline import Pipeline
-from scipy.sparse import csr_matrix
-import numpy as np
-import matplotlib.pyplot as plt
 import time
+from sklearn.ensemble import ExtraTreesClassifier
+from sklearn.feature_selection import SelectFromModel 
+from sklearn.pipeline import Pipeline
+# Custom file imports
+from preprocess.filetokenizer import readRows
+from preprocess.sparsifier import sparsify
+from crossvalidation.crossvalidation import *
 
 def main():
     startTime = time.time()
-
+    # Retrieve data
     trainingRows, labels = readRows("./data/train_drugs.data", loadFile=True, isTrainingFile=True)
     testRows, _ = readRows("./data/test_drugs.data", loadFile=True, isTrainingFile=False)
-    lengths = [len(r) for r in trainingRows]
-    maxLength = max(lengths)
-    #[r.extend([0]*(maxLength - len(r))) for r in trainingRows]
-    """
-        1) Convert filtered sparse matrix into original sparse matrix
-        2) 
-
-    """
     
-    csrTup = ([], [])
-    csrData = []
-    for index, row in enumerate(trainingRows):
-        for idxToken in row:
-            csrTup[0].append(index)
-            csrTup[1].append(int(idxToken))
-            csrData.append(1)
+    # Convert data into csr matrix
+    sMatrix = sparsify(trainingRows)
     
-    sMatrix = csr_matrix((csrData, csrTup))
+    # Define feature-selection estimators
+    featureEstimator = ExtraTreesClassifier(n_estimators=50)
+    featureSelectionModel = SelectFromModel(featureEstimator)
 
-    etc = ExtraTreesClassifier(n_estimators=50)
-    #lsvc = LinearSVC(C=0.01, penalty="l1", dual=False).fit(sMatrix, labels)
-    #lrcv = LogisticRegressionCV(Cs=10, dual=False, random_state=0, scoring='f1').fit(trainingRows,labels)
-    #print(lrcv.scores_)
+    # Initialize classifier pipeline with feature selection model
+    classifierPipeline = Pipeline([('feature_selection', featureSelectionModel)])
 
-    #model = VarianceThreshold(threshold=(.8 * (1 - .8)))
-    model = SelectFromModel(etc)#SelectFromModel(lsvc)
+    # newton-cg, lbfgs, and sag ONLY support l2, while all 5 support l1
+    solverMap = {'l1': ['liblinear', 'saga'], 
+                 'l2': ['liblinear', 'newton-cg', 'lbfgs', 'sag', 'saga'] }
 
-    """
-        Test each type of solver.
-        sag and saga tend to have convergence warnings (tested up to max_iter=5000 (default is 100))
-    """
-    solverScores = []
-    for s in ['newton-cg', 'lbfgs', 'liblinear', 'sag', 'saga']:
-        for l in ['l1', 'l2']:
-            #newton-cg, lbfgs, and sag ONLY support l2, while all 5 support l1
-            if ( (l == 'l1' and (s == 'liblinear' or s == 'saga')) or l == 'l2'):
-                logReg = LogisticRegression(solver=s, penalty=l, max_iter=10000, multi_class='ovr')
-                #logReg = LogisticRegression(solver='liblinear', penalty = 'l2')
-                #logReg = LogisticRegressionCV(solver='liblinear', penalty='l1', Cs=10, cv=2, dual=False, random_state=0, scoring='f1')
-                clf = Pipeline([
-                    ('feature_selection', model),
-                    ('classification', logReg)
-                ])
-                clf.fit(sMatrix, labels)
-
-                """
-                    5x2 fold cross validation: avg of 5 sets of 2-fold CVs
-                """
-                avgs = []
-                #for _ in range(5):
-                #    scores = logRegCrossValidation(clf, sMatrix, labels, 2, 'f1')
-                #    avgs.append( np.average(scores) )
-
-                for _ in range(5):
-                    scores = logRegCrossValidation(clf, sMatrix, labels, 2, 'f1')
-                    avgs.append( np.average(scores) )
-
-                avg = np.average(avgs)
-                print("\nSOLVER: " + l + " " + s)
-                print("avgs: " + str(avgs))
-                print("avg score from 5x2 cv: %f\n" % avg)
-                solverScores.append((l + " " + s, avg))
-
-                #print(clf.score(sMatrix, labels)) #<- seems to always return 1.0, may have to do with UndefinedMetricWarning
-    solverScores = sorted(solverScores, key=lambda x: x[1])
+    # Perform cross validation 
+    solverScores = logRegCVScore(classifierPipeline, solverMap, sMatrix, labels)
+    
     print("\nFinal scores: ")
     for tup in solverScores[::-1]:
         print(tup[0] + ": \t%f" % tup[1])
